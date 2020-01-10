ccb57eb3dc68442a4a930f0f00f77a6a630d4bdb
NakagamiYuta <guguru0014@gmail.com>
2020-01-10 20:04:14 +0900

    [add] TsunamiSimulator.java
    コンストラクタ実装

---
+++ TsunamiSimulator.java
@@ -0,0 +1,14 @@
+public class TsunamiSimulator {
+
+    // 定数
+    static final double grav = 9.8;
+
+    // その他
+    String depthFile;
+    double x[], depth[];
+
+    public TsunamiSimulator(String depthFile) {
+        this.depthFile = depthFile;
+    }
+
+}
