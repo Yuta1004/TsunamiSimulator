8bc5e572ee4d7722208f913ad80a48614f543331
NakagamiYuta <guguru0014@gmail.com>
2020-01-10 23:13:56 +0900

    [add] 1ステップシミュレータを進める処理を実装

--- TsunamiSimulator.java
+++ TsunamiSimulator.java
@@ -14,6 +14,7 @@ public class TsunamiSimulator implements Iterable<StepData>, Iterator<StepData>{
     static final int M = 60;
     static final int S = 1;
     static final double grav = 9.8;
+    static final double eps = 0.01;
 
     // 時間データ
     int clock = 0*H + 0*M + 0*S;
@@ -152,6 +153,51 @@ public class TsunamiSimulator implements Iterable<StepData>, Iterator<StepData>{
             x[idx] = Double.parseDouble(line.split("\t")[0]);       // x        (str->double)
             x[idx] *= 1000;                                         // km->m
             depth[idx] = Double.parseDouble(line.split("\t")[1]);   // depth    (str->double)
+        }
+    }
+
+    private void step() {
+        // 1. 未来ステップ値計算
+        // 1-1. 水平流速更新
+        for(int idx = 0; idx < dataSize-1; ++ idx) {
+            if(step == 0)
+                uf[idx] = up[idx] - grav*(dt/dx)*(zp[idx+1]-zp[idx]);
+            else
+                uf[idx] = ub[idx] - 2*grav*(dt/dx)*(zp[idx+1]-zp[idx]);
+        }
+        // 1-2. 海面変位更新
+        for(int idx = 1; idx < dataSize-1; ++ idx) {
+            double depth0 = (depth[idx]+depth[idx-1]) * 0.5;
+            double depth1 = (depth[idx]+depth[idx+1]) * 0.5;
+            if(step == 0)
+                zf[idx] = zp[idx] - (dx/dx)*(depth1*up[idx]-depth0*up[idx-1]);
+            else
+                zf[idx] = zb[idx] - 2*(dt/dx)*(depth1*up[idx]-depth0*up[idx-1]);
+        }
+
+        // 2. 陸上で流速を0にする
+        for(int idx = 0; idx < dataSize-1; ++ idx) {
+            if((depth[idx]+depth[idx+1])*0.5 <= 0)
+                uf[idx] = 0;
+        }
+
+        // 3. 沖側に伝わる津波を強制的に減衰させる
+        for(int idx = dataSize-50; idx < dataSize; ++ idx) {
+            zf[idx] *= (dataSize-idx)/50.0;
+            uf[idx] *= (dataSize-idx)/50.0;
+        }
+
+        // 4. 境界条件セット
+        zf[0] = 0;
+        zf[dataSize-1] = 0;
+        if(step == 0)
+            return;
+
+        // 5. 計算安定化処理
+        for(int idx = 0; idx < dataSize; ++ idx) {
+            if(idx < dataSize-1)
+                up[idx] += eps * (uf[idx]-2*up[idx]+ub[idx]);
+            zp[idx] += eps * (zf[idx]-2*zp[idx]+zb[idx]);
         }
     }
 
