2732e36e1687ef127d3a6686d1dbe977b52b196d
NakagamiYuta <guguru0014@gmail.com>
2020-01-10 21:52:24 +0900

    [add] ステップごとのデータを扱うデータクラスを作成

---
+++ StepData.java
@@ -0,0 +1,13 @@
+public class StepData {
+
+    public int clock, step;
+    public double x[], z[];
+
+    public StepData(int clock, int step, double x[], double z[]) {
+        this.clock = clock;
+        this.step = step;
+        this.x = x;
+        this.z = z;
+    }
+
+}
