9dc892215700ff7ce74cc8b1af51063439e499bf
NakagamiYuta <guguru0014@gmail.com>
2020-01-11 14:21:14 +0900

    [change] シミュレータの初期化処理をMainのコンストラクタにおいた

--- java/Main.java
+++ java/Main.java
@@ -1,19 +1,18 @@
 class Main {
 
-    public static void main(String args[]) {
-        // 初期設定
-        TsunamiSimulator simulator = new TsunamiSimulator("../data/DEPTH.data");
+    TsunamiSimulator simulator;
+
+    public Main() {
+        // シミュレータ設定
+        simulator = new TsunamiSimulator("../data/DEPTH.data");
         simulator.setClock(12, 0, 0);           // 時計を12:00:00に
         simulator.setSimulateTime(3, 0, 0);     // 3時間分シミュレートする
         simulator.setItrTimeStep(0, 1, 0);      // 1分間隔でデータを取得する
         simulator.setWaveHeight(115, -2);       // 115mの場所に-2mの波
         simulator.setWaveHeight(215, 5);        // 215mの場所に5mの波
+    }
 
-        // シミュレート
-        for(StepData data: simulator) {
-            data.print();
-            System.out.println();
-        }
+    public static void main(String args[]) {
     }
 
 }
