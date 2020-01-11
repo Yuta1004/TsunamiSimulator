493bc76168f0429fa691d7be9a0ecf5b363bce57
NakagamiYuta <guguru0014@gmail.com>
2020-01-11 15:04:37 +0900

    [add] とりあえず津波を描画できるようにした

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,10 @@
 import javafx.application.Application;
+import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
-import javafx.stage.Stage;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.XYChart;
 
 public class Main extends Application {
 
@@ -9,7 +12,9 @@ public class Main extends Application {
     private final int WIDTH = 1280, HEIGHT = 720;
     private final String TITLE = "Tsunami Simulator v0.0.1";
 
-    // その他
+    // 描画用
+    private Group root;
+    private AreaChart<Number, Number> areaChart;
     private TsunamiSimulator simulator;
 
     /**
@@ -34,7 +39,10 @@ public class Main extends Application {
     @Override
     public void start(Stage stage){
         stage = setupStage(stage);
+        setupAreaChart();
         stage.show();
+
+        drawTsunami();
     }
 
     /**
@@ -44,12 +52,40 @@ public class Main extends Application {
      * @return Stage 設定済みStage
      */
     private Stage setupStage(Stage stage) {
-        Group root = new Group();
+        root = new Group();
         Scene scene = new Scene(root, WIDTH, HEIGHT);
         stage.setScene(scene);
         stage.setTitle(TITLE);
         return stage;
     }
 
+    /**
+     * AreaChartの初期設定を行う
+     */
+    private void setupAreaChart() {
+        // x, y軸
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Disttance(km)");
+        yAxis.setLabel("Height(m)");
+
+        // グラフ設定
+        areaChart = new AreaChart<>(xAxis, yAxis);
+        areaChart.setMinWidth(1280);
+        areaChart.setMinHeight(720);
+        root.getChildren().add(areaChart);
+    }
+
+    /**
+     * 津波を描画する
+     */
+    private void drawTsunami() {
+        StepData data = simulator.next();
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        for(int idx = 0; idx < data.x.length; ++ idx) {
+            series.getData().add(new XYChart.Data<Number, Number>(data.x[idx]/1000, data.z[idx]));
+        }
+        areaChart.getData().add(series);
+    }
 
 }
