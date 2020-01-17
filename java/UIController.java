fd352870fc1cd0b70453d096e3642a2311d14aff
NakagamiYuta <guguru0014@gmail.com>
2020-01-17 18:14:53 +0900

    [add] AreaChartの初期化処理を追加

--- java/UIController.java
+++ java/UIController.java
@@ -3,10 +3,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import lib.NegativeBGAreaChart;
 import Tsunami.TsunamiSimulator;
 import Tsunami.TsunamiSimulatorEvenness;
 import Tsunami.TsunamiSimulatorUnevenness;
@@ -18,12 +22,15 @@ public class UIController implements Initializable {
     private TsunamiSimulator simulator;
     private static final int EVENNESS = 0;
     private static final int UNEVENNESS = 1;
+    private NegativeBGAreaChart<Number, Number> tsunamiChart;
 
     // UI部品
     @FXML
     private Label clockLabel, modeLabel;
     @FXML
     private MenuItem setEvenness, setUnevenness;
+    @FXML
+    private AnchorPane chartPane;
 
     /**
      * 初期化処理
@@ -31,6 +38,7 @@ public class UIController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // 初期化
+        initAreaChart();
         initSimulator((simulatorMode = EVENNESS));
 
         // UI部品にonActionを載せる
@@ -57,4 +65,30 @@ public class UIController implements Initializable {
         simulator.setSimulateTime(6, 0, 0);     // シミュレート時間 => 6時間
     }
 
+    /**
+     * AreaChartを初期化する(NegativeBGAreaChart)
+     */
+    private void initAreaChart() {
+        // x, y軸
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Distance(km)");
+        yAxis.setLabel("Height(m)");
+        yAxis.setAutoRanging(false);
+        yAxis.setLowerBound(-10);
+        yAxis.setUpperBound(20);
+
+        // グラフ設定
+        tsunamiChart = new NegativeBGAreaChart<>(xAxis, yAxis);
+        tsunamiChart.setCreateSymbols(false);
+        tsunamiChart.setAnimated(false);
+
+        // AnchorPane設定
+        AnchorPane.setTopAnchor(tsunamiChart, 10.0);
+        AnchorPane.setLeftAnchor(tsunamiChart, 10.0);
+        AnchorPane.setRightAnchor(tsunamiChart, 10.0);
+        AnchorPane.setBottomAnchor(tsunamiChart, 10.0);
+        chartPane.getChildren().clear();
+        chartPane.getChildren().add(tsunamiChart);
+    }
 }
