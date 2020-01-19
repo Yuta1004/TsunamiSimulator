635431d0a637b47f794f59ee1e9a402bfe65dce0
NakagamiYuta <guguru0014@gmail.com>
2020-01-19 14:43:41 +0900

    [fix] 画面サイズの調整処理を加えた

--- java/Main.java
+++ java/Main.java
@@ -2,6 +2,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.Screen;
+import javafx.geometry.Rectangle2D;
 
 import java.io.IOException;
 
@@ -16,14 +18,20 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            // Window size
+            Rectangle2D d = Screen.getPrimary().getVisualBounds();
+            int width = (int)Math.min(1280, d.getWidth());
+            int height = (int)Math.min(800, d.getHeight());
+
             // Scene
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainUI.fxml"));
             loader.setController(new MainUIController());
-            Scene scene = new Scene(loader.load());
+            Scene scene = new Scene(loader.load(), width, height);
 
             // Stage
             stage.setTitle("Tsunami Simulator Java Edition");
             stage.setScene(scene);
+            stage.setResizable(false);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
